df7b9ed618b63953e1032f48dedcabd1dd37c2d6
Your Name <sanjayshreeram77@gmail.com>
2024-01-21 18:12:48 +0530

    added changes

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,89 @@
+"use client";
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useState,useEffect } from 'react'
+import {signIn,signOut,useSession,getProviders} from 'next-auth/react'
 
 const Nav = () => {
+    const IsUserLoggedIn=true;
+    const [providers,setproviders]=useState<any>(null);
+
+
+    useEffect(()=>{
+        const set=async()=>{
+            const response=await getProviders();
+            setproviders(response);
+        }
+        set()
+
+    },[])
   return (
-    <div>Nav</div>
+   <nav className='flex-between w-full mb-16 pt-3'>
+    <Link href="/" className='flex gap-2 flex-center'>
+        <Image src='/assets/images/logo.svg' alt="Promptopia Logo" width={30} height={30} className='object-contain'/>
+    <p className='logo_text uppercase'>
+        Promptopia
+    </p>
+    </Link>
+    {/* {desktop navigation } */}
+    <div className='sm:flex hidden'>
+        {IsUserLoggedIn ?(
+
+            <div className='flex gap-3 md:gap-5'>
+                <Link href="/create-prompt" className='black_btn'>
+                Create Post
+                </Link>
+                <button type='button' onClick={()=>signOut} className='outline_btn'>
+                    Sign Out
+                </button>
+                <Link href="/profile" className='flex gap-2 flex-center'>
+                    <Image src='/assets/images/logo.svg' alt="User Icon" width={35} height={35} className='object-contain'/>
+                    <p className='logo_text uppercase'>
+                        Profile
+                    </p>
+                    </Link>
+
+                </div>
+        ):
+        (
+            <>
+            {
+                providers && Object.values(providers).map((provider:any)=>(
+
+           <button type='button' key={provider.name} onClick={()=>signIn(provider.id)} className='black_btn'>
+        Sign In
+           </button>
+
+                ))
+            }
+
+
+            </>
+        )
+
+        }
+
+    </div>
+     {/* {mobile navigation} */}
+    <div className='sm:hidden flex relative'>
+        {IsUserLoggedIn ?
+        
+           (
+            <div>
+                </div>
+
+           )
+
+           :(
+            <>
+            </>
+           )
+    }
+
+    </div>
+
+   </nav>
   )
 }
 
