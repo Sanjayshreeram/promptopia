998101704f09ac2b570efc946a5e0596323ff4ea
Your Name <sanjayshreeram77@gmail.com>
2024-01-26 20:05:03 +0530

    added changes

---
+++ app/api/prompt/new/route.tsx
@@ -0,0 +1,13 @@
+import { connectToDatabase } from "@utils/database";
+export const POST=async (req,res)=>{
+    const {userId,prompt,tag}= await req.json();
+
+    try{
+        await connectToDatabase();
+         
+    }
+    catch(error){
+        
+    }
+    
+}
