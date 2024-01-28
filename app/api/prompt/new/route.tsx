fbcdfa6df94863fa379d79058838565d304cdfc7
Your Name <sanjayshreeram77@gmail.com>
2024-01-28 11:19:00 +0530

    created prompt schema

--- app/api/prompt/new/route.tsx
+++ app/api/prompt/new/route.tsx
@@ -1,9 +1,26 @@
 import { connectToDatabase } from "@utils/database";
+import Prompt from "@models/prompt";
+
 export const POST=async (req:any)=>{
-    const {userId,prompt,tag}= await req.json();
+    const {userId,prompt,tag}= await req.json(); //extracting data from backend
 
     try{
-        await connectToDatabase();
+        await connectToDatabase(); //connect to database
+
+        const newprompt=new Prompt({
+
+            creator:userId,
+            prompt,
+            tag
+
+  
+        })  //creating a new prompt using old
+
+
+        await newprompt.save();
+
+        return new  Response(JSON.stringify(newprompt),{status:201})
+          
          
     }
     catch(error){
