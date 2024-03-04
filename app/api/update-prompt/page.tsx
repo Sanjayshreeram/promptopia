1dbc047077696838f8f2750ed6e10646d484d0b8
Your Name <sanjayshreeram77@gmail.com>
2024-03-04 21:15:08 +0530

    made chmages in feed component

--- app/api/update-prompt/page.tsx
+++ app/api/update-prompt/page.tsx
@@ -2,14 +2,17 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useRouter,useSearchParams } from 'next/navigation';
 import Form from '@components/Form';
 import Link from 'next/link'; // Import Link component
 
 
 const EditPrompt = () => {
+    console.log('called edit');
     const router = useRouter();
-    const { data: session } = useSession();
+   
+    const searchparams=useSearchParams();
+    const promptId:any=searchparams.get('id');
 
     const [submitting, setSubmitting] = useState<boolean>(false); 
     const [post, setPost] = useState({
@@ -18,46 +21,69 @@ const EditPrompt = () => {
     });
 
     useEffect(()=>{
+        const getpromptdetails=async ()=>{
+            const res:any=await fetch(`/api/prompt/${promptId}`);
+            const data:any=await res.json();
 
+            setPost({prompt:data.prompt,
+                tag:data.tag
+      
+      })
 
-    },[])
-
-    const handleCreatePrompt = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setSubmitting(true);
-        //this will fetch data from ->api->prompt->new
-
-        try {
-            const res = await fetch('/api/prompt/new', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    prompt: post.prompt,
-                    userId: session?.user?.id, // Use optional chaining to safely access 'id' property
-                    tag: post.tag,
-                })
-            });
-
-            const json = await res.json();
-            if (!res.ok) {
-                throw Error(json.message);
-            }
-            alert('Prompt created successfully');
-            router.push('/');
-        } finally {
-            setSubmitting(false);
         }
-    };
+
+
+
+       
+        if(promptId)
+        getpromptdetails();
+
+
+    },[promptId])
+
+    const meesage=(s:any)=>{
+        console.log('logged',s);
+    }
+
+    // const handleCreatePrompt = async (e: React.FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault();
+        // setSubmitting(true);
+        // //this will fetch data from ->api->prompt->new
+
+        // try {
+        //     const res = await fetch('/api/prompt/new', {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify({
+        //             prompt: post.prompt,
+        //             userId: session?.user?.id, // Use optional chaining to safely access 'id' property
+        //             tag: post.tag,
+        //         })
+    //         });
+
+    //         const json = await res.json();
+    //         if (!res.ok) {
+    //             throw Error(json.message);
+    //         }
+    //         alert('Prompt created successfully');
+    //         router.push('/');
+    //     } finally {
+    //         setSubmitting(false);
+    //     }
+    // };
 
     return (
         <Form
-            type="Create"
+            type="Edit"
             post={post}
             setPost={setPost}
             submitting={submitting}
-            handleSubmit={handleCreatePrompt} // Renamed handlesubmit to handleSubmit
+            handleSubmit={async (e: React.FormEvent<HTMLFormElement>) => { 
+                e.preventDefault();
+                console.log("Form submitted"); 
+            }}
         />
     );
 }
